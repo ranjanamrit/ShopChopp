a24462076b53700e8bbeb088a6319c3ad93ef5e8
ranjanamrit <amrit.ranjan.work@gmail.com>
2021-07-17 21:05:10 +0530

    --added:admin->client-> deleteAll feature

--- client/src/Components/MainPages/products/Products.js
+++ client/src/Components/MainPages/products/Products.js
@@ -11,12 +11,27 @@ export default function Products() {
     const [token] = state.token
     const [callBack,setcallBack] = state.ProductAPI.callBack
     const [loading,setLoading] = useState(false)
+    const [isCheck, setisCheck] = useState(false)
 
 
     const handleCheck = (id) => {
-        console.log(id)
+        products.forEach(product => {
+            if(product._id === id) product.checked = !product.checked
+        });
+        setProducts([...products])
     }
-
+    const checkAll = () => {
+        products.forEach(product => {
+            product.checked = !isCheck
+        })
+        setProducts([...products])
+        setisCheck(!isCheck)
+    }
+    const deleteAll = () => {
+        products.forEach(product => {
+            if(product.checked) deleteItem(product._id,product.images.public_id)
+        })
+    }
     const deleteItem = async (id,public_id) => {
         try {
             setLoading(true)
@@ -28,15 +43,24 @@ export default function Products() {
             })
             await destroyImg
             await deleteProduct
-            setLoading(false)
             setcallBack(!callBack)
+            setLoading(false)
+
         } catch (err) {
             alert(err.response.data.msg)
         }
     }
-    if(loading) return <div className="product"> <Loading></Loading></div>
+    if(loading) return <div> <Loading></Loading></div>
     return (
         <>
+        {
+            isAdmin &&
+            <div className="delete-all">
+                <span>Select all</span>
+                <input type="checkbox" checked={isCheck} onChange={checkAll}/>
+                <button onClick={deleteAll}>Delete All</button>
+            </div>
+        }
         <div className="products">
             {
                 products.map(product => {
